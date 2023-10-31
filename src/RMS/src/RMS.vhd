0eca0e780d032df5432be8687c654e7b6d7b813d
jmhafele <jmhafele@iastate.edu>
2023-10-31 16:39:51 -0500

    RMS module initial written

--- src/RMS/src/RMS.vhd
+++ src/RMS/src/RMS.vhd
@@ -168,19 +168,20 @@ architecture mixed of RMS is
             o_F  : out std_logic_vector(7 downto 0)); -- 32 bit data value output
     end component;
 
+    -- Timer Counter
+    signal s_current_time : std_logic_vector(31 downto 0);
+    signal s_LCM_clear    : std_logic;
+
     -- Control Module
-    signal s_task_sel : std_logic_vector(2 downto 0);
+    signal s_task_sel    : std_logic_vector(2 downto 0);
+    signal s_task_sel_WE : std_logic_vector(2 downto 0);
+
+    -- Decoder
+    signal s_next_PC_WE : std_logic_vector(7 downto 0); --Use lower 5 bits
 
     -- Task State Reg Internal Wires
-    signal s_next_PC_WE   : std_logic_vector(4 downto 0);
     signal s_Period_Clear : std_logic_vector(4 downto 0);
     signal s_isComplete   : std_logic_vector(4 downto 0);
-
-    signal s_task0_next_PC : std_logic_vector(31 downto 0);
-    signal s_task1_next_PC : std_logic_vector(31 downto 0);
-    signal s_task2_next_PC : std_logic_vector(31 downto 0);
-    signal s_task3_next_PC : std_logic_vector(31 downto 0);
-    signal s_task4_next_PC : std_logic_vector(31 downto 0);
 
     signal s_task0_current_PC : std_logic_vector(31 downto 0);
     signal s_task1_current_PC : std_logic_vector(31 downto 0);
@@ -201,7 +202,57 @@ architecture mixed of RMS is
     signal s_dffN_DATA_MUX : std_logic_vector(31 downto 0);
     signal s_dffN_DATA_WB  : std_logic_vector(31 downto 0);
 
+    -- PC Task Adder
+    signal s_PC_DATA_MUX : std_logic_vector(31 downto 0);
+    signal s_PC_DATA_WB  : std_logic_vector(31 downto 0);
+
 begin
+
+    g_timerCounter : timerCounter
+    generic map(
+        N   => 32,
+        LCM => LCM
+    )
+    port map(
+        i_CLK      => i_CLK,
+        i_RST      => i_Asynch_RST,
+        i_WE       => '1',            --Comes from decoder
+        o_count    => s_current_time, --Output of adder from selected task
+        o_LCMclear => s_LCM_clear);   --Output of DFF
+
+    -- FIXME CONTROL
+    g_TaskControl : TaskControl
+    generic map(
+        TASK0PERIOD => TASK0PERIOD,
+        TASK1PERIOD => TASK1PERIOD,
+        TASK2PERIOD => TASK2PERIOD,
+        TASK3PERIOD => TASK3PERIOD,
+        TASK4PERIOD => TASK4PERIOD
+    )
+    port map(
+        i_CLK                 => i_CLK,
+        i_Asynch_RST          => i_Asynch_RST,
+        i_LCM_Clear           => s_LCM_clear,
+        i_Task0_Complete      => s_isComplete(0),
+        i_Task1_Complete      => s_isComplete(1),
+        i_Task2_Complete      => s_isComplete(2),
+        i_Task3_Complete      => s_isComplete(3),
+        i_Task4_Complete      => s_isComplete(4),
+        i_Current_Time        => s_current_time,
+        o_Current_Task_Sel    => s_task_sel,
+        o_Task0_Period_Clear  => s_Period_Clear(0),
+        o_Task1_Period_Clear  => s_Period_Clear(1),
+        o_Task2_Period_Clear  => s_Period_Clear(2),
+        o_Task3_Period_Clear  => s_Period_Clear(3),
+        o_Task4_Period_Clear  => s_Period_Clear(4),
+        o_Current_Task_Sel_WE => s_task_sel_WE
+    );
+
+    g_decoder_3t8 : decoder_3t8
+    port map(
+        i_A  => s_task_sel,
+        i_WE => s_task_sel_WE,
+        o_F  => s_next_PC_WE);
 
     g_task0_TaskStateReg : TaskStateReg
     generic map(
@@ -211,7 +262,7 @@ begin
     port map(
         i_CLK          => i_CLK,
         i_Asynch_RST   => i_Asynch_RST,
-        i_next_PC      => s_task0_next_PC,
+        i_next_PC      => s_PC_DATA_WB,
         i_next_PC_WE   => s_next_PC_WE(0),
         i_Period_Clear => s_Period_Clear(0),
         o_current_PC   => s_task0_current_PC,
@@ -225,7 +276,7 @@ begin
     port map(
         i_CLK          => i_CLK,
         i_Asynch_RST   => i_Asynch_RST,
-        i_next_PC      => s_task1_next_PC,
+        i_next_PC      => s_PC_DATA_WB,
         i_next_PC_WE   => s_next_PC_WE(1),
         i_Period_Clear => s_Period_Clear(1),
         o_current_PC   => s_task1_current_PC,
@@ -239,7 +290,7 @@ begin
     port map(
         i_CLK          => i_CLK,
         i_Asynch_RST   => i_Asynch_RST,
-        i_next_PC      => s_task2_next_PC,
+        i_next_PC      => s_PC_DATA_WB,
         i_next_PC_WE   => s_next_PC_WE(2),
         i_Period_Clear => s_Period_Clear(2),
         o_current_PC   => s_task2_current_PC,
@@ -253,7 +304,7 @@ begin
     port map(
         i_CLK          => i_CLK,
         i_Asynch_RST   => i_Asynch_RST,
-        i_next_PC      => s_task3_next_PC,
+        i_next_PC      => s_PC_DATA_WB,
         i_next_PC_WE   => s_next_PC_WE(3),
         i_Period_Clear => s_Period_Clear(3),
         o_current_PC   => s_task3_current_PC,
@@ -267,7 +318,7 @@ begin
     port map(
         i_CLK          => i_CLK,
         i_Asynch_RST   => i_Asynch_RST,
-        i_next_PC      => s_task4_next_PC,
+        i_next_PC      => s_PC_DATA_WB,
         i_next_PC_WE   => s_next_PC_WE(4),
         i_Period_Clear => s_Period_Clear(4),
         o_current_PC   => s_task0_current_PC,
@@ -278,7 +329,7 @@ begin
         i_CLK => i_CLK,
         i_RST => i_Asynch_RST,
         i_WE  => s_dffN_WE(0),    --Comes from decoder
-        i_D   => s_dffN_DATA,     --Output of adder from selected task
+        i_D   => s_dffN_DATA_WB,  --Output of adder from selected task
         o_Q   => s_task0_dffN_Q); --Output of DFF
 
     g_task1_dffN : dffN
@@ -286,7 +337,7 @@ begin
         i_CLK => i_CLK,
         i_RST => i_Asynch_RST,
         i_WE  => s_dffN_WE(1),    --Comes from decoder
-        i_D   => s_dffN_DATA,     --Output of adder from selected task
+        i_D   => s_dffN_DATA_WB,  --Output of adder from selected task
         o_Q   => s_task1_dffN_Q); --Output of DFF
 
     g_task2_dffN : dffN
@@ -294,7 +345,7 @@ begin
         i_CLK => i_CLK,
         i_RST => i_Asynch_RST,
         i_WE  => s_dffN_WE(2),    --Comes from decoder
-        i_D   => s_dffN_DATA,     --Output of adder from selected task
+        i_D   => s_dffN_DATA_WB,  --Output of adder from selected task
         o_Q   => s_task2_dffN_Q); --Output of DFF
 
     g_task3_dffN : dffN
@@ -302,7 +353,7 @@ begin
         i_CLK => i_CLK,
         i_RST => i_Asynch_RST,
         i_WE  => s_dffN_WE(3),    --Comes from decoder
-        i_D   => s_dffN_DATA,     --Output of adder from selected task
+        i_D   => s_dffN_DATA_WB,  --Output of adder from selected task
         o_Q   => s_task3_dffN_Q); --Output of DFF
 
     g_task4_dffN : dffN
@@ -310,7 +361,7 @@ begin
         i_CLK => i_CLK,
         i_RST => i_Asynch_RST,
         i_WE  => s_dffN_WE(4),    --Comes from decoder
-        i_D   => s_dffN_DATA,     --Output of adder from selected task
+        i_D   => s_dffN_DATA_WB,  --Output of adder from selected task
         o_Q   => s_task4_dffN_Q); --Output of DFF
 
     g_dffN_MUX : mux_8t1_32b
@@ -328,9 +379,31 @@ begin
 
     g_dffN_ripple : ripple_adder
     port map(
-        i_X    => i_CLK,
-        i_Y    => i_Asynch_RST,
+        i_X    => s_dffN_DATA_MUX,
+        i_Y    => i_dffN_incr,
         i_Cin  => '0',
-        o_S    => s_dffN_DATA,
+        o_S    => s_dffN_DATA_WB,
         o_Cout => open);
+
+    g_PC_MUX : mux_8t1_32b
+    port map(
+        i_S  => s_task_sel,
+        i_D0 => s_task0_current_PC,
+        i_D1 => s_task1_current_PC,
+        i_D2 => s_task2_current_PC,
+        i_D3 => s_task3_current_PC,
+        i_D4 => s_task4_current_PC,
+        i_D5 => X"00000000", --Not Used
+        i_D6 => X"00000000", --Not Used
+        i_D7 => X"00000000", --Not Used
+        o_O  => s_PC_DATA_MUX);
+
+    g_PC_ripple : ripple_adder
+    port map(
+        i_X    => s_PC_DATA_MUX,
+        i_Y    => i_PC_incr,
+        i_Cin  => '0',
+        o_S    => s_PC_DATA_WB,
+        o_Cout => open);
+
 end mixed;
